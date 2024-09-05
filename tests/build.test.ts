103b65a93616719a17dce972f33c68815b790406
rainbowatcher <rainbow-w@qq.com>
2024-09-05 12:05:16 +0800

    test: fix windows compability issue

--- tests/build.test.ts
+++ tests/build.test.ts
@@ -10,12 +10,17 @@ import type { CommandLineArgs, ConfigOptions } from "../src/util"
 
 
 describe.concurrent("arg parse", () => {
+    const JSON_CONFIG = path.join("fixture", "configs", "config.json")
+    const MJS_CONFIG = path.join("fixture", "configs", "config.mjs")
+    const CJS_CONFIG = path.join("fixture", "configs", "config.cjs")
+    const TS_CONFIG = path.join("fixture", "configs", "config.ts")
+    const entries = [path.join("fixture", "less")]
+
     afterAll(async () => {
-        await writeFile("fixture/configs/config.json", "{}")
+        await writeFile(JSON_CONFIG, "{}")
     })
 
     it("should equal to default", async () => {
-        const entries = ["fixture/less"]
         const cliArgs: CommandLineArgs = {}
         const resolvedOptions = await resolveOptions(entries, cliArgs)
         expect(resolvedOptions).toStrictEqual({
@@ -25,130 +30,121 @@ describe.concurrent("arg parse", () => {
         })
     })
 
-    it("config options should overwrite default option", async () => {
-        const config = "fixture/configs/config.json"
-        const entries = ["fixture/less"]
-        await writeFile(config, JSON.stringify({
-            clean: true,
-        }))
-        const cliArgs: CommandLineArgs = {
-            config,
-        }
-        const result = await resolveOptions(entries, cliArgs)
-        expect(result).toStrictEqual({
-            ...DEFAULT_BUILD_OPTIONS,
-            clean: true,
-            config: path.join(process.cwd(), config),
-            entry: entries.map(entry => path.join(process.cwd(), entry)),
-            output: path.join(process.cwd(), DEFAULT_BUILD_OPTIONS.output),
+    describe.sequential("json config", () => {
+        it("config options should overwrite default option", async () => {
+            await writeFile(JSON_CONFIG, JSON.stringify({
+                clean: true,
+            }))
+            const cliArgs: CommandLineArgs = {
+                config: JSON_CONFIG,
+            }
+            const result = await resolveOptions(entries, cliArgs)
+            expect(result).toStrictEqual({
+                ...DEFAULT_BUILD_OPTIONS,
+                clean: true,
+                config: path.join(process.cwd(), JSON_CONFIG),
+                entry: entries.map(entry => path.join(process.cwd(), entry)),
+                output: path.join(process.cwd(), DEFAULT_BUILD_OPTIONS.output),
+            })
         })
-    })
 
-    it.sequential("should merge with config", async () => {
-        const config = "fixture/configs/config.json"
-        await writeFile(config, JSON.stringify({
-            clean: true,
-            opt: {
-                optLevel: "2",
-                shrinkLevel: "1",
-            },
-            release: false,
-        } satisfies ConfigOptions))
-        const entries = ["fixture/less"]
-        const cliArgs: CommandLineArgs = {
-            config,
-        }
-        const resolvedOptions = await resolveOptions(entries, cliArgs)
-        expect(resolvedOptions).toStrictEqual({
-            ...DEFAULT_BUILD_OPTIONS,
-            clean: true,
-            config: path.join(process.cwd(), config),
-            entry: entries.map(entry => path.join(process.cwd(), entry)),
-            opt: {
-                optLevel: "2",
-                shrinkLevel: "1",
-            },
-            output: path.join(process.cwd(), DEFAULT_BUILD_OPTIONS.output),
+        it("should merge with config", async () => {
+            await writeFile(JSON_CONFIG, JSON.stringify({
+                clean: true,
+                opt: {
+                    optLevel: "2",
+                    shrinkLevel: "1",
+                },
+                release: false,
+            } satisfies ConfigOptions))
+            const cliArgs: CommandLineArgs = {
+                config: JSON_CONFIG,
+            }
+            const resolvedOptions = await resolveOptions(entries, cliArgs)
+            expect(resolvedOptions).toStrictEqual({
+                ...DEFAULT_BUILD_OPTIONS,
+                clean: true,
+                config: path.join(process.cwd(), JSON_CONFIG),
+                entry: entries.map(entry => path.join(process.cwd(), entry)),
+                opt: {
+                    optLevel: "2",
+                    shrinkLevel: "1",
+                },
+                output: path.join(process.cwd(), DEFAULT_BUILD_OPTIONS.output),
+            })
         })
-    })
 
-    it.sequential("cli args priority should higher than config", async () => {
-        const config = "fixture/configs/config.json"
-        const entries = ["fixture/less"]
-        await writeFile(config, JSON.stringify({
-            clean: true,
-            release: false,
-        } satisfies ConfigOptions))
-        const args: CommandLineArgs = {
-            clean: false,
-            config,
-            release: true,
-        }
-        const result = await resolveOptions(entries, args)
-        expect(result).toStrictEqual({
-            ...DEFAULT_BUILD_OPTIONS,
-            clean: false,
-            config: path.join(process.cwd(), config),
-            entry: entries.map(entry => path.join(process.cwd(), entry)),
-            output: path.join(process.cwd(), DEFAULT_BUILD_OPTIONS.output),
-            release: true,
+        it("cli args priority should higher than config", async () => {
+            await writeFile(JSON_CONFIG, JSON.stringify({
+                clean: true,
+                release: false,
+            } satisfies ConfigOptions))
+            const args: CommandLineArgs = {
+                clean: false,
+                config: JSON_CONFIG,
+                release: true,
+            }
+            const result = await resolveOptions(entries, args)
+            expect(result).toStrictEqual({
+                ...DEFAULT_BUILD_OPTIONS,
+                clean: false,
+                config: path.join(process.cwd(), JSON_CONFIG),
+                entry: entries.map(entry => path.join(process.cwd(), entry)),
+                output: path.join(process.cwd(), DEFAULT_BUILD_OPTIONS.output),
+                release: true,
+            })
         })
-    })
 
-    it.sequential("array args should be overwrite", async () => {
-        const config = "fixture/configs/config.json"
-        const entries = ["fixture/less"]
-        await writeFile(config, JSON.stringify({
-            extensions: ["cjs", "mts", "wasm"],
-        } satisfies ConfigOptions))
-        const cliArgs: CommandLineArgs = {
-            config,
-        }
-        const result = await resolveOptions(entries, cliArgs)
-        expect(result).toStrictEqual({
-            ...DEFAULT_BUILD_OPTIONS,
-            config: path.join(process.cwd(), config),
-            entry: entries.map(entry => path.join(process.cwd(), entry)),
-            extensions: ["cjs", "mts", "wasm"],
-            output: path.join(process.cwd(), DEFAULT_BUILD_OPTIONS.output),
+        it("array args should be overwrite", async () => {
+            await writeFile(JSON_CONFIG, JSON.stringify({
+                extensions: ["cjs", "mts", "wasm"],
+            } satisfies ConfigOptions))
+            const cliArgs: CommandLineArgs = {
+                config: JSON_CONFIG,
+            }
+            const result = await resolveOptions(entries, cliArgs)
+            expect(result).toStrictEqual({
+                ...DEFAULT_BUILD_OPTIONS,
+                config: path.join(process.cwd(), JSON_CONFIG),
+                entry: entries.map(entry => path.join(process.cwd(), entry)),
+                extensions: ["cjs", "mts", "wasm"],
+                output: path.join(process.cwd(), DEFAULT_BUILD_OPTIONS.output),
+            })
         })
-    })
 
-    it.sequential("cli without specify entires", async () => {
-        const config = "fixture/configs/config.json"
-        const entries: string[] = []
-        await writeFile(config, JSON.stringify({
-            entry: ["fixture/less"],
-            extensions: ["cjs", "mts", "wasm"],
-        } satisfies ConfigOptions))
-        const cliArgs: CommandLineArgs = {
-            config,
-        }
-        const result = await resolveOptions(entries, cliArgs)
+        it("when entires specified in config not cli", async () => {
+            const emptyEntries: string[] = []
+            await writeFile(JSON_CONFIG, JSON.stringify({
+                entry: entries,
+                extensions: ["cjs", "mts", "wasm"],
+            } satisfies ConfigOptions))
+            const cliArgs: CommandLineArgs = {
+                config: JSON_CONFIG,
+            }
+            const result = await resolveOptions(emptyEntries, cliArgs)
 
-        expect(result).toStrictEqual({
-            ...DEFAULT_BUILD_OPTIONS,
-            config: path.join(process.cwd(), config),
-            entry: ["fixture/less"].map(entry => path.join(process.cwd(), entry)),
-            extensions: ["cjs", "mts", "wasm"],
-            output: path.join(process.cwd(), DEFAULT_BUILD_OPTIONS.output),
+            expect(result).toStrictEqual({
+                ...DEFAULT_BUILD_OPTIONS,
+                config: path.join(process.cwd(), JSON_CONFIG),
+                entry: entries.map(entry => path.join(process.cwd(), entry)),
+                extensions: ["cjs", "mts", "wasm"],
+                output: path.join(process.cwd(), DEFAULT_BUILD_OPTIONS.output),
+            })
         })
     })
 
     it("can load ts config", async () => {
-        const config = "fixture/configs/config.ts"
-        const entries = ["fixture/less"]
         const cliArgs: CommandLineArgs = {
-            config,
+            config: TS_CONFIG,
         }
-        await writeFile(config, `export default ${JSON.stringify({
-            entry: ["fixture/less"],
+        await writeFile(TS_CONFIG, `export default ${JSON.stringify({
+            entry: entries,
             extensions: ["cjs", "mts", "wasm"],
         } satisfies ConfigOptions)}`)
         const result = await resolveOptions(entries, cliArgs)
         expect(result).toStrictEqual({
             ...DEFAULT_BUILD_OPTIONS,
-            config: path.join(process.cwd(), config),
+            config: path.join(process.cwd(), TS_CONFIG),
             entry: entries.map(entry => path.join(process.cwd(), entry)),
             extensions: ["cjs", "mts", "wasm"],
             output: path.join(process.cwd(), DEFAULT_BUILD_OPTIONS.output),
@@ -156,19 +152,17 @@ describe.concurrent("arg parse", () => {
     })
 
     it("can load mjs config", async () => {
-        const config = "fixture/configs/config.mjs"
-        const entries = ["fixture/less"]
         const cliArgs: CommandLineArgs = {
-            config,
+            config: MJS_CONFIG,
         }
-        await writeFile(config, `export default ${JSON.stringify({
-            entry: ["fixture/less"],
+        await writeFile(MJS_CONFIG, `export default ${JSON.stringify({
+            entry: entries,
             extensions: ["mjs", "wasm"],
         } satisfies ConfigOptions)}`)
         const result = await resolveOptions(entries, cliArgs)
         expect(result).toStrictEqual({
             ...DEFAULT_BUILD_OPTIONS,
-            config: path.join(process.cwd(), config),
+            config: path.join(process.cwd(), MJS_CONFIG),
             entry: entries.map(entry => path.join(process.cwd(), entry)),
             extensions: ["mjs", "wasm"],
             output: path.join(process.cwd(), DEFAULT_BUILD_OPTIONS.output),
@@ -176,19 +170,17 @@ describe.concurrent("arg parse", () => {
     })
 
     it("can load cjs config", async () => {
-        const config = "fixture/configs/config.cjs"
-        const entries = ["fixture/less"]
         const cliArgs: CommandLineArgs = {
-            config,
+            config: CJS_CONFIG,
         }
-        await writeFile(config, `module.exports = ${JSON.stringify({
-            entry: ["fixture/less"],
+        await writeFile(CJS_CONFIG, `module.exports = ${JSON.stringify({
+            entry: entries,
             extensions: ["cjs", "wasm"],
         } satisfies ConfigOptions)}`)
         const result = await resolveOptions(entries, cliArgs)
         expect(result).toStrictEqual({
             ...DEFAULT_BUILD_OPTIONS,
-            config: path.join(process.cwd(), config),
+            config: path.join(process.cwd(), CJS_CONFIG),
             entry: entries.map(entry => path.join(process.cwd(), entry)),
             extensions: ["cjs", "wasm"],
             output: path.join(process.cwd(), DEFAULT_BUILD_OPTIONS.output),
