1754d080521974771926a40d207cd903b081e2b6
rainbowatcher <rainbow-w@qq.com>
2024-09-06 11:15:31 +0800

    test: add test case for node to run wasm

---
+++ tests/e2e/node.test.ts
@@ -0,0 +1,16 @@
+import fg from "fast-glob"
+import { describe, expect, it } from "vitest"
+
+describe("node", () => {
+    it("should run with node", async () => {
+        expect.hasAssertions()
+        const globs = fg.sync("wasm-dist/**/index.js")
+        const promises = globs.map(async (indexPath) => {
+            expect(indexPath).toBeTruthy()
+            const module = await import(indexPath ?? "")
+            await module.default()
+            expect(module.hello_world()).toBe("Hello, World!")
+        })
+        await Promise.all(promises)
+    })
+})
