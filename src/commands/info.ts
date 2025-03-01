75c262c1b8e5368eae4b049a92044260db576cf3
rainbowatcher <rainbow-w@qq.com>
2025-03-01 14:11:07 +0800

    feat: remove wasm-opt in info command

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -7,7 +7,6 @@ import type { CommandLineArgs } from "../util"
 const execa = execaSync({ all: true, reject: false })
 
 const WASM_PACK = "wasm-pack"
-const WASM_OPT = "wasm-opt"
 
 export async function printSystemInfo(_opts: CommandLineArgs) {
     console.log("arch: %s", process.arch)
@@ -16,15 +15,9 @@ export async function printSystemInfo(_opts: CommandLineArgs) {
     console.log("cpus: %O", os.cpus().length)
     console.log("mem: %s", `${os.totalmem() / 1024 / 1024 / 1024}G`)
     const wasmPackExists = await commandExists(WASM_PACK)
-    const wasmOptExists = await commandExists(WASM_OPT)
-    let wasmOptVersion = "",
-        wasmPackVersion = ""
+    let wasmPackVersion = ""
     if (wasmPackExists) {
         wasmPackVersion = execa(WASM_PACK, ["--version"]).all.replace("wasm-pack ", "")
     }
-    if (wasmOptExists) {
-        wasmOptVersion = execa(WASM_OPT, ["--version"]).all.replace("wasm-opt ", "")
-    }
     console.log("wasm-pack: %s", wasmPackVersion)
-    console.log("wasm-opt: %s", wasmOptVersion)
 }
