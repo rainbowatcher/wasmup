f22b10b7e08d016a7c96b8fe4de20a55c173e3a9
rainbowatcher <rainbow-w@qq.com>
2025-03-01 14:12:05 +0800

    feat: remove wasm-opt in install command

--- src/util/github.ts
+++
@@ -1,5 +0,0 @@
-export async function getLatestRelease(owner: string, repo: string): Promise<any> {
-    const url = `https://api.github.com/repos/${owner}/${repo}/releases/latest`
-    return fetch(url)
-        .then(res => res.json() as Promise<any>)
-}
