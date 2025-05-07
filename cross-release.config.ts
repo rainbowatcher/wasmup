1b7c59b8a8fc1fe70ddf7bed4a719ee8d4bc6ee1
rainbowatcher <rainbow-w@qq.com>
2025-05-07 13:12:19 +0800

    chore: remove cross-release configuration file and update TypeScript settings
    - Remove configuration file cross-release.config.ts from the project
    - Update tsconfig.json to exclude cross-release.config.ts from the list of included files
    
    Signed-off-by: rainbowatcher <rainbow-w@qq.com>

--- cross-release.config.ts
+++
@@ -1,9 +0,0 @@
-import process from "node:process"
-import { defineConfig } from "cross-release-cli"
-
-export default defineConfig({
-    cwd: process.cwd(),
-    push: {
-        followTags: true,
-    },
-})
