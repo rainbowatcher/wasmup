b5630e12896b9598dad75e7c6617d9fae8e3b0c3
rainbowatcher <rainbow-w@qq.com>
2025-06-26 11:26:42 +0800

    refactor: simplify greeting function parameter and formatting logic
    - Change the function parameter from a reference to JsValue to a String for simplicity
    - Update the function to format a greeting message with the provided string argument
    
    Signed-off-by: rainbowatcher <rainbow-w@qq.com>

--- fixture/hello_world/src/lib.rs
+++ fixture/hello_world/src/lib.rs
@@ -1,6 +1,6 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn hello_world(a: &JsValue) -> String {
-    format!("Hello, {:?}!", a)
+pub fn hello_world(a: String) -> String {
+    format!("Hello, {}!", a)
 }
